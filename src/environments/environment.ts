1b462f3a0105f22df306c09cf68d3bf10fd986e1
Gustavo Cabezal <40568482+meisterveda@users.noreply.github.com>
2023-03-11 16:44:17 -0600

    added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'loki-1dc1c',
+    appId: '1:113961533467:web:3e5d459113858f5119f8be',
+    storageBucket: 'loki-1dc1c.appspot.com',
+    apiKey: 'AIzaSyCTNTbnKjHMumeA0Zk8mYVqzQz979EWF3I',
+    authDomain: 'loki-1dc1c.firebaseapp.com',
+    messagingSenderId: '113961533467',
+    measurementId: 'G-2DSKL442W7',
+  }
+};
