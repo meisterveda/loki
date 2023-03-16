2c7a5b72bc8e47e1ec51f758abb42f4d69cbdd5c
Gustavo Cabezal <40568482+meisterveda@users.noreply.github.com>
2023-03-16 18:02:30 -0500

    modiefied structure

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from 'src/app/service/auth.service';
+import { AuthService } from 'src/app/service/auth/auth.service';
 
 @Component({
   templateUrl: './login.component.html',
